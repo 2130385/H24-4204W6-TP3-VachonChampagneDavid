536fd1780ebc970fd8462068950df5cd53460dfd
David <2130385@cegepmontpetit.ca>
2024-04-04 11:11:12 -0400

    FCT: implémtentation du Token JWT
    - Ajout des controlleurs, modèles et DTO pour Scores et Utilisateurs
    - Ajout des différentes requêtes

---
+++ TP3/Data/WebAPITP3context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using TP3.Models;
+
+namespace Labo8api.Data
+{
+    public class WebAPITP3context : IdentityDbContext<User>
+    {
+        public WebAPITP3context(DbContextOptions<WebAPITP3context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TP3.Models.Score> Score { get; set; } = default!;
+    }
+}
---
+++ TP3/Models/Score.cs
@@ -0,0 +1,11 @@
+﻿namespace TP3.Models
+{
+    public class Score
+    {
+        public int Id { get; set; }
+
+        public string Value { get; set; }
+
+        public virtual User? User { get; set; }
+    }
+}
